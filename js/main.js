cad2579f25d4f7e041d2da9621431a547434ee65
Joe Bader <{ID}+{username}@users.noreply.github.com>
2018-08-16 07:09:48 +0000

    Visual elements and animations

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$(function() {
 
+    $(document).on('scrollstart', function() {
+        console.log('scroll started')
+    })
+  
+    $(document).on('scrollend', function() {
+        console.log('scroll ended')
+    })
+  
+})
--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,29 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+// jQuery scroll start stop
+(function() {
+  
+    var lastScrollAt = Date.now()
+      , timeout
+  
+    function scrollStartStop() {
+        var $this = $(this)
+      
+        if (Date.now() - lastScrollAt > 100)
+            $this.trigger('scrollstart')
+          
+        lastScrollAt = Date.now()
+      
+        clearTimeout(timeout)
+      
+        timeout = setTimeout(function() {
+            if (Date.now() - lastScrollAt > 99)
+            $this.trigger('scrollend')
+        }, 100)
+    }
+    
+    $(document).on('scroll', scrollStartStop)
+    
+  })()
